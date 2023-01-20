b8a8af32f43cdf9a3b6d27a03e63d599f4d2a48d
Pedro Henrique <pedrohv20fernandes@gmail.com>
2023-01-20 17:49:20 -0300

    Melhorando o mobile e rotas da aplicação

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -4,6 +4,7 @@ import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-yea
 
 import { HabitDay, DAY_SIZE } from '../components/HabitDay'
 import { Header } from '../components/Header'
+import { useNavigation } from '@react-navigation/native'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const datesFromYearStart = generateDatesFromYearBeginning()
@@ -11,6 +12,7 @@ const minimumSummaryDatesSizes = 18 * 7
 const amountOfDatesToFill = minimumSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
+  const { navigate } = useNavigation()
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -25,13 +27,17 @@ export function Home() {
           </Text>
         ))}
       </View>
-      <ScrollView 
+      <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       >
+
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date, index) => (
-            <HabitDay key={`${date}-${index}`} />
+            <HabitDay 
+              key={`${date}-${index}`} 
+              onPress={() => navigate('Habit', { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDatesToFill > 0 &&
